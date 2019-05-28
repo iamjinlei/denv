7724b9fd4dd7a5d19406f242f6fb25f8f4400711
Lei Jin <jinl@8btc.com>
2019-05-28 16:59:10 +0800

    update

--- unix_eth.sh
+++ unix_eth.sh
@@ -14,7 +14,8 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 PS1="ssh" source ~/.bashrc
 
 nvm install node
-npm install -g truffle
+# Got keccak build error on truffle@^5.0.0
+npm install -g truffle@4.1.15
 
 #pushd ~/tmp && curl -sL https://rpm.nodesource.com/setup_10.x | bash && yum install -y nodejs && popd
 # npm install ganache-cli web3@1.0.0-beta.37
